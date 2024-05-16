baba6e8607561d957bc54bb72ec5460b2770b907
Kufre Akpan <contactkufreakpan@gmail.com>
2024-05-16 11:16:01 -0700

    trying to fix issues

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 Create a Flask app: app_views and register the blueprint
 """
 from os import getenv
+from flask import Flask
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
@@ -11,11 +12,9 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
 def teardown_engine(exception):
     """
-
     """
     storage.close()
 
@@ -23,14 +22,13 @@ def teardown_engine(exception):
 @app.errorhandler(404)
 def not_found(error):
     """
-
     """
 
     response = {"error": "Not found"}
     return jsonify(response), 404
 
-
 if __name__ == '__main__':
     Host = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(debug=True, host=Host, port=PORT, threaded=True)
+    app.run(host=Host, port=PORT, threaded=True)
+    app.run(debug = True, host=Host, port=PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,19 +5,11 @@ Create a Flask app: app_views
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
-
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
